032eafaa0e4a23a6bf631515185082addd126fa6
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-01-13 15:53:33 -0300

    Completed Section 31

--- SECTION 31 - RESTFUL/app.js
+++ SECTION 31 - RESTFUL/app.js
@@ -21,8 +21,13 @@ app.listen(3000, function () {
     console.log('server running on port 3000');
 });
 
-//First - Get method, fetches all the articles.
-app.get('/articles', function (req, res) {
+/*
+*First - Get method, fetches all the articles.
+*Second - Post method, creates one new article
+*Third - Delete all the articles 
+*/
+
+app.route('/articles').get(function (req, res) {
     Article.find({}, function (err, articlesCollection) {
         if (!err) {
             res.send(articlesCollection);
@@ -30,10 +35,7 @@ app.get('/articles', function (req, res) {
             res.send(err);
         }
     });
-});
-
-//Second - Post method, creates one new article
-app.post('/articles', function (req, res) {
+}).post(function (req, res) {
     const newArticle = new Article({
         title: req.body.title,
         content: req.body.content
@@ -45,10 +47,7 @@ app.post('/articles', function (req, res) {
             res.send(err);
         }
     });
-});
-
-// Third - Delete all the articles
-app.delete('/articles', function (req, res) {
+}).delete(function (req, res) {
     Article.deleteMany({}, function (err) {
         if (!err) {
             res.send("Succesfully deleted all articles");
@@ -57,3 +56,60 @@ app.delete('/articles', function (req, res) {
         }
     });
 });
+
+/*
+*fourth - especific route for get element. 
+*fifth - especific route for put.
+*sixth - especific route for patch.
+*seventh - especific route for delete
+*/
+app.route('/articles/:articleTitle').get(function (req, res) {
+    const articleTitle = req.params.articleTitle;
+    Article.findOne({ title: articleTitle }, function (err, foundArticle) {
+        if (!err) {
+            if (foundArticle === null) {
+                res.send("Article title not found");
+            } else {
+                res.send(foundArticle);
+            }
+        } else {
+            console.log(err)
+        }
+    })
+}).put(function (req, res) {
+    const articleTitle = req.params.articleTitle;
+    Article.update({ title: articleTitle },
+        { title: req.body.title, content: req.body.content },
+        { overwrite: true },
+        function (err, updatedArticle) {
+            if (!err) {
+                if (updatedArticle === null) {
+                    res.send("Article not found")
+                } else {
+                    res.send("Article Updated")
+                }
+            } else {
+                res.send(err)
+            }
+        })
+}).patch(function (req, res) {
+    const articleTitle = req.params.articleTitle;
+    Article.update({ title: articleTitle },
+        { $set: req.body },
+        function (err) {
+            if (!err) {
+                res.send("Successfully updated article");
+            } else {
+                res.send(err)
+            }
+        })
+}).delete(function (req, res) { 
+    const articleTitle = req.params.articleTitle;
+    Article.deleteOne({title: articleTitle}, function(err){
+        if(!err){
+            res.send('Successfully Deleted');
+        }else{
+            res.send(err);
+        }
+    })
+ });
