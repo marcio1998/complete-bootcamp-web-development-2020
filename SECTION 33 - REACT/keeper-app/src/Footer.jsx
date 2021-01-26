8977aa44885e7490ba67e66bc929b9c42fc10b42
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-01-26 14:44:27 -0300

    Initial Commit section 33

---
+++ SECTION 33 - REACT/keeper-app/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>&copy; Marcio Gabreil G. Mengali - 2021</p>
+        </footer>
+    )
+}
+
+export default Footer;
